4df32ee6bfefb6b1e6572822a15e94ef5d906340
Elisabet Gonzalez <eli.jvn@gmail.com>
2025-09-08 11:16:50 -0600

    Fix mobile menu closing and correct styles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const navMenu = document.getElementById("nav-menu");
 const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
+const navLink = document.querySelectorAll(".nav___menu-link");
 
 //Show menu
 if(navToggle) {
@@ -14,3 +15,8 @@ if(navClose) {
         navMenu.classList.remove("show-menu");
     })
 }
+
+//Close menu when a nav link is clicked
+navLink.forEach(n => n.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+}));
